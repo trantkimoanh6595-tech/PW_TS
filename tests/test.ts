a3debb7e67bf68fff31fbd2afd93788b0d9f698d
oanh.ttk <trantkimoanh6595@gmail.com>
2026-07-16 11:06:01 +0900

    Test

---
+++ tests/test.ts
@@ -0,0 +1,15 @@
+
+type User = {
+    id: number;
+    userName: string;
+    email: string;
+
+};
+const userProfile: User ={
+    id: 65,
+    userName: "Oanh",
+    email: "onhashchange.ttk@gmail.com"
+}
+const userId= userProfile.id
+
+console.log(userId);
